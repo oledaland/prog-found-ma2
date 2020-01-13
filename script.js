37b65118cc8ef4d405ef577bf6b225780f4b73e6
oledaland <57751683+oledaland@users.noreply.github.com>
2020-01-13 10:31:54 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+// Task 1
+var petArray = ["cat", "cow", "dog"];
+
+// Task 2
+console.log(petArray[1]);
+
+// Task 3
+petArray.push("sheep");
+
+// Task 4
+console.log(petArray.length);
+
+// Task 5
+var catObject = {
+  name: "Ole",
+  colour: "Green",
+  age: 21
+}
+
+// Task 6
+console.log(catObject.age);
+
+// Task 7
+var catArray = ["Ole", "Green", 21];
+
+// Task 8
+var numberOfProperties = catArray.length;
+
+for (var i = 0; i < numberOfProperties; i++){
+    console.log(catArray[i]);
+}
+
+// Task 9
+function logToConsole(MyNumberOneArgumentYey){
+  console.log("My number one value for my function");
+  console.log("Donkey");
+}
+
+// Task 10
+logToConsole();
